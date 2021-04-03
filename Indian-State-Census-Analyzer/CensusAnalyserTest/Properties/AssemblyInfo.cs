ed0a76eaf873069e7fd768789700e23361775541
prashikjaware <prashikjaware123@gmail.com>
2021-04-03 13:18:07 +0530

    [Prashik]Added: Ability for the analyser to load the Indian States Census Information from a csv file

---
+++ Indian-State-Census-Analyzer/CensusAnalyserTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CensusAnalyserTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CensusAnalyserTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0caf50ac-9664-49bf-8125-39be7959a83d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Indian-State-Census-Analyzer/CensusAnalyserTest/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using IndianStateCensusAnalyzer;
+using System.Collections.Generic;
+using NUnit.Framework;
+using IndianStateCensusAnalyzer.DTO;
+using static IndianStateCensusAnalyzer.CensusAnalyser;
+
+namespace CensusAnalyserTest
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class UnitTest1
+    {
+        public class Tests
+        {
+            //CensusAnalyser.CensusAnalyser censusAnalyser;
+
+            static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+            static string indianStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
+            static string indianStateCensusFilePath = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\IndiaStateCensusData.csv";
+            static string wrongHeaderIndianCensusFilePath = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\WrongIndiaStateCensusData.csv";
+            static string delimiterIndianCensusFilePath = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\DelimiterIndiaStateCensusData.csv";
+            static string wrongIndianStateCensusFilePath = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\WrongIndiaStateCensusData1.csv";
+            static string wrongIndianStateCensusFileType = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\IndiaStateCensusData.txt";
+            static string indianStateCodeFilePath = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\IndiaStateCode.csv";
+            static string wrongIndianStateCodeFileType = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\IndiaStateCode.txt";
+            static string delimiterIndianStateCodeFilePath = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\DelimiterIndiaStateCode.csv";
+            static string wrongHeaderStateCodeFilePath = @"C:\Users\Prashik Jaware\source\repos\DesignPrinciples\Indian-State-Census-Analyzer\CensusAnalyserTest\CsvFiles\WrongIndiaStateCode1.csv";
+            //US Census FilePath
+            static string usCensusHeaders = "State Id,State,Population,Housing units,Total area,Water area,Land area,Population Density,Housing Density";
+            static string usCensusFilepath = @"C:\Users\Dell\source\repos\CensusAnalyser\CensusAnalyserTest\CsvFiles\USCsvFiles\USCensusData.csv";
+            static string wrongUSCensusFilePath = @"C:\Users\Dell\source\repos\CensusAnalyser\CensusAnalyserTest\CsvFiles\USCsvFiles\USData.csv";
+            static string wrongUSCensusFileType = @"C:\Users\Dell\source\repos\CensusAnalyser\CensusAnalyserTest\CsvFiles\USCsvFiles\USCensusData.txt";
+            static string wrongHeaderUSCensusFilePath = @"C:\Users\Dell\source\repos\CensusAnalyser\CensusAnalyserTest\CsvFiles\USCsvFiles\WrongHeaderUSCensusData.csv";
+            static string delimeterUSCensusFilePath = @"C:\Users\Dell\source\repos\CensusAnalyser\CensusAnalyserTest\CsvFiles\USCsvFiles\DelimiterUSCensusData.csv";
+
+            CensusAnalyser censusAnalyser;
+            Dictionary<string, CensusDTO> totalRecord;
+            Dictionary<string, CensusDTO> stateRecord;
+
+            [SetUp]
+            public void Setup()
+            {
+                censusAnalyser = new CensusAnalyser();
+                totalRecord = new Dictionary<string, CensusDTO>();
+                stateRecord = new Dictionary<string, CensusDTO>();
+            }
+
+            [Test]
+            public void Test1()
+            {
+                Assert.Pass();
+            }
+
+            [Test]
+            public void GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount()
+            {
+                totalRecord = censusAnalyser.LoadCensusData(indianStateCensusFilePath, Country.INDIA, indianStateCensusHeaders);
+                Assert.AreEqual(29, totalRecord.Count);
+            }
+
+
+        }
+
+    }
+}
